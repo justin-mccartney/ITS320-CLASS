e73311fdbb319c7ee4639195117e5666bca493bc
justin-mccartney <justin.mccartney@csuglobal.edu>
2024-04-20 16:42:37 -0500

    Commit #1

---
+++ ITS320-CTA1-OPTION2.py
@@ -0,0 +1,11 @@
+# Reading integers in from the user ...
+print("Enter an integer: ")
+a = int(input())
+print("Enter a second integer: ")
+b = int(input())
+
+# Printing results for all operations ...
+print("Outputs:")
+print(a // b)   #Integer division
+print(a / b)    #Float division
+print(a % b)    #Modulo division
