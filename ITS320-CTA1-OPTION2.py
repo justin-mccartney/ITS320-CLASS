528a507ae1f99baac9f35b7db59a9e560c4fdae7
justin-mccartney <justin.mccartney@csuglobal.edu>
2024-05-23 17:37:14 -0500

    Commit #5.5

--- ITS320-CTA1-OPTION2.py
+++ ITS320-CTA1-OPTION2.py
@@ -1,3 +1,25 @@
+# ------------------------------
+# Program Name: CTA1-OPTION2
+# Author: Justin McCartney
+# Date: 4-20-2024
+# ------------------------------
+# Pseudocode: [Below]
+"""
+print(enter an integer)
+    a = INPUT
+print(enter another integer)
+    b = INPUT
+
+print(OUTPUTS)
+    print(a // b) - integer division
+    print(a / b) - float division
+    print(a % b) - modulo division
+"""
+# ------------------------------
+# Inputs: two individual integers
+# Outputs: output the outcome of... integer division, float division, and modulo division
+# (as well as the prompts for user input and the heading for the output)
+# ------------------------------
 # Reading integers in from the user ...
 print("Enter an integer: ")
 a = int(input())
